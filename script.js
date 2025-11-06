2af6a603b498d5f10d1006e24e101f6024c84719
Mohammadreza_Shahbazi <EMINEMENGLAND2000@gmail.com>
2025-11-06 11:05:33 +0330

    Add js,html,css files.

---
+++ script.js
@@ -0,0 +1,62 @@
+let currentInput = '';
+let operation = '';
+let firstOperand = null;
+
+function appendNumber(number) {
+  currentInput += number.toString();
+  document.getElementById('display').value = currentInput;
+}
+
+function setOperation(op) {
+  if (currentInput === '') return;
+
+  if (firstOperand === null) {
+    firstOperand = parseFloat(currentInput);
+  } else {
+    calculate();
+  }
+
+  operation = op;
+  currentInput = '';
+}
+
+function calculate() {
+  if (firstOperand === null || currentInput === '') return;
+
+  let secondOperand = parseFloat(currentInput);
+  let result;
+
+  switch (operation) {
+    case '+':
+      result = firstOperand + secondOperand;
+      break;
+    case '-':
+      result = firstOperand - secondOperand;
+      break;
+    case '*':
+      result = firstOperand * secondOperand;
+      break;
+    case '/':
+      result = secondOperand !== 0
+        ? firstOperand / secondOperand
+        : 'Division by zero is not possible.';
+      break;
+    default:
+      return;
+  }
+
+  document.getElementById('result').innerText = 'Result: ' + result;
+  resetCalculator();
+}
+
+function clearDisplay() {
+  resetCalculator();
+  document.getElementById('display').value = '';
+  document.getElementById('result').innerText = '';
+}
+
+function resetCalculator() {
+  currentInput = '';
+  operation = '';
+  firstOperand = null;
+}
